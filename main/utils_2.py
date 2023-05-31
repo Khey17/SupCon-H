b227a13c827548b9f2fede1b628914bbf01d7f75
Karthikheyaa Kurra <90310297+Khey17@users.noreply.github.com>
2023-05-31 09:45:59 +0530

    Updated and fixed the errors in FERPair Class
    This class inherits the data from ImageFolder, So it doesn't work like cifar100 where the data is loaded (pre-built)

--- main/utils_2.py
+++ main/utils_2.py
@@ -1,11 +1,15 @@
 import os
+import torch
 from typing import Any, Callable, Optional
 from PIL import Image
 from torchvision import transforms, datasets
+from torchvision.datasets import ImageFolder
 import cv2
 import numpy as np
 from torchvision.datasets.folder import default_loader
+from collections import defaultdict
 from random import sample
+import random
 
 
 class FERPair(datasets.ImageFolder):
@@ -41,35 +45,46 @@ class FERPair(datasets.ImageFolder):
         return len(self.images)
 
 
-# creating pairs of positive samples for ferplus
-class FERPair_true_label(datasets.ImageFolder):
-    # randomly sampling the pairs of positive samples
+class FERPair_true_label(ImageFolder):
     def __init__(self, root, train=True, transform=None):
-        super().__init__(root=root, train=train, transform=transform)
-        def get_labels(i):
-            return [i for i in range(len(self)) if self.targets[i]==1]
+        mode = "train" if train else "test"
+        data_dir = os.path.join(root, "FERPlus", mode)
+        super().__init__(data_dir, transform=transform)
+        self.images, self.labels = self._load_data()
         
-        self.label_index = [get_labels(i) for i in range(8)]
+    def _load_data(self):
+        images = []
+        labels = []
+        
+        for img_path, label in self.samples:
+            img = Image.open(img_path).convert("RGB")
+            images.append(img)
+            labels.append(label)
+        
+        return images, labels
+    
+    def _get_label_index(self):
+        label_index = defaultdict(list)
+        
+        for idx, label in enumerate(self.labels):
+            label_index[label].append(idx)
+        
+        return label_index
+    
+    def __getitem__(self, index):
+        img, target = self.images[index], self.labels[index]
+        
+        if self.transform is not None:
+            img = self.transform(img)
+        
+        pos_1 = img
+        pos_2_idx = random.choice(self._get_label_index()[target])
+        pos_2 = self.transform(self.images[pos_2_idx])
+        
+        return pos_1, pos_2, target
 
-        def __getitem__(self, i):
-            img1, target = self.data[i], self.targets[i]
-
-            i_example_same_label = sample(self.label_index[self.targets[i]], 1)[0]
-            img2 = self.data[i_example_same_label]
-
-            img1 = Image.fromarray(img1)
-            img2 = Image.fromarray(img2)
-
-            if self.transform is not None:
-                pos_1 = self.transform(img1)
-                pos_2 = self.transform(img2)
-
-            if self.target_transform is not None:
-                target = self.target_transform(target)
-
-            return pos_1, pos_2, target
-
-
+    def __len__(self):
+        return len(self.images)
 
 
 class GaussianBlur(object):
@@ -107,12 +122,12 @@ test_transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
 
-def get_dataset(dataset_name, M_view, N_view, root='C:/Users/Karth/Downloads/H-SCL-main/data/', pair=True):
+def get_dataset(dataset_name, M_view, N_view, root='./data/', pair=True):
     if pair:
         if dataset_name == 'ferplus':
-            train_data = FERPair(root="./data", train=True, transform=train_transform)
-            memory_data = FERPair(root="./data", train=True, transform=test_transform)
-            test_data = FERPair(root="./data", train=False, transform=test_transform)
+            train_data = FERPair(root=root, train=True, transform=train_transform)
+            memory_data = FERPair(root=root, train=True, transform=test_transform)
+            test_data = FERPair(root=root, train=False, transform=test_transform)
 
         elif dataset_name == 'ferpair_true_label':
             train_data = FERPair_true_label(root=root, train=True, transform=train_transform)
@@ -123,9 +138,9 @@ def get_dataset(dataset_name, M_view, N_view, root='C:/Users/Karth/Downloads/H-S
             raise Exception('Invalid dataset name')
     else:
         if dataset_name in ['ferplus', 'ferpair_true_label']:
-            train_data = FERPair(root="./data", train=True, transform=train_transform)
-            memory_data = FERPair(root="./data", train=True, transform=test_transform)
-            test_data = FERPair(root="./data", train=False, transform=test_transform)
+            train_data = FERPair(root=root, train=True, transform=train_transform)
+            memory_data = FERPair(root=root, train=True, transform=test_transform)
+            test_data = FERPair(root=root, train=False, transform=test_transform)
         else:
             raise Exception('Invalid dataset name')
 
